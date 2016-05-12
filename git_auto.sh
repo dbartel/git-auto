1744589b868c2eb4cf19a64fb2895f8810e7c04c
Daniel Bartel <daniel.bartel@bottlerocketstudios.com>
2016-05-12 13:24:15 -0500

    Thu May 12 13:24:15 CDT 2016 auto-commit

--- git_auto.sh
+++ git_auto.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Checks if changes exist, commits them, and pushes them to a remote
+# Use with cron for a primitive file sync
+
 GIT_DIR="."
 
 pushd $GIT_DIR
+
 git diff-files --quiet
 if [[ $? != 0 ]]
 then
     git commit -a -m "`date` auto-commit"
+    # Push branch
+    if [ `git branch --list origin/master`]
+    then
+        git push origin master
+    fi
 fi
 
