0c0e2184a98bf406a38fb7329a87d709d9de5fe5
daniel bartel <dan.m.bartel@gmail.com>
2016-05-12 21:05:11 -0500

    Thu May 12 21:05:11 CDT 2016 auto-commit

--- git_auto.sh
+++ git_auto.sh
@@ -3,18 +3,34 @@
 # Checks if changes exist, commits them, and pushes them to a remote
 # Use with cron for a primitive file sync
 
-GIT_DIR="."
-
-pushd $GIT_DIR
-
-git diff-files --quiet
-if [[ $? != 0 ]]
-then
+# Sync files with remote
+sync_files() {
     git commit -a -m "`date` auto-commit"
     # Push branch
-    if [ `git branch --list --remote origin/master` ]
+    if [ `git branch --list --remote origin/$BRANCH` ]
     then
         git push origin master
-    fi
+    fi  
+}
+
+# Repo to clone
+REPO_DIR=$1
+
+# Branch we're syncing
+BRANCH=$2
+
+# Go to our dir
+pushd $REPO_DIR
+
+# Check if files have changed
+git diff-files --quiet
+
+#Sync if necessary
+if [[ $? != 0 ]]
+then
+    sync_files
 fi
+
+popd
+exit 0
 
